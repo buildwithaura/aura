5a914ab1239d4042925a521153ee96d75380e207
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-23 03:22:53 +0800

    Merge the watermark extension into app/.

--- config/extensions.rb
+++ config/extensions.rb
@@ -4,9 +4,9 @@
 Main.configure do |m|
   # These are the extensions that are to be loaded.
   # You don't need to worry about these; try not to override this.
-  m.set :core_extensions, %w(editor)
+  m.set :core_extensions, %w(editor watermark)
 
   # Here are more extensions to be loaded.
   # These here are meant to be overloaded.
-  m.set :additional_extensions, %w(watermark contact_form default_theme)
+  m.set :additional_extensions, %w(default_theme)
 end
