8c9ab2f80a01ccabae925d107526114fbaf1e4b7
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-29 19:11:13 +0800

    Fix Sass support.

--- config/scss.rb
+++ config/scss.rb
@@ -1,11 +1,12 @@
 class Main
   set :scss, {
     :load_paths => [
-      root('app/css'),
-      Aura.files.glob('extensions/*/css'),
-      approot('app/css')
+      Aura.gem_root('app/css'),
+      Aura.root('app/css')
     ].flatten
   }
   set :scss, self.scss.merge(:line_numbers => true, :debug_info => true, :always_check => true) if self.development?
   set :scss, self.scss.merge(:style => :compressed) if self.production?
+
+  set :sass, scss
 end
