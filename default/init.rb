7e79bc50deb42b0c2561af9af7d03c8c0e9b9e5b
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-29 23:49:02 +0800

    Add a Bundler warning in the default app.

--- default/init.rb
+++ default/init.rb
@@ -2,7 +2,14 @@ ENV['APP_FILE'] = __FILE__
 
 # Gems
 require "rubygems"  unless defined?(::Gem)
-require "bundler"
-Bundler.setup
+
+begin
+  require "bundler"
+  Bundler.setup
+rescue LoadError => e
+  $stderr << "\nYou will need Bundler to run this application. Try:\n"
+  $stderr << "$ gem install bundler\n\n"
+  exit
+end
 
 require "aura/app"
