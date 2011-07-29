2cd032700afd344a45a098c01222d13be9feb069
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-29 13:55:35 +0800

    Revert "Use Google's CDN for jQuery even on development."
    This reverts commit bfac6642f3ff27a173ab1e3a048fb561887b91f0.

--- app/helpers/jquery_helpers.rb
+++ app/helpers/jquery_helpers.rb
@@ -1,16 +1,24 @@
 class Main
   module JqueryHelpers
     def jquery(path='/js/jquery.js', version='1.4.3')
-      # Grab Google CDN's jQuery; fallback to local if necessary
-      url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
-      "<script type='text/javascript' src=\"#{url}\"></script>"+
-      "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
+      if settings.production?
+        # Grab Google CDN's jQuery; fallback to local if necessary
+        url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
+        "<script type='text/javascript' src=\"#{url}\"></script>"+
+        "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
+      else
+        "<script type='text/javascript' src='#{path}'></script>"
+      end
     end
 
     def jquery_ui(path='/js/jqueryui.js', version='1.8.5')
-      url = "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
-      "<script type='text/javascript' src=\"#{url}\"></script>"+
-      "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
+      if settings.production?
+        url = "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
+        "<script type='text/javascript' src=\"#{url}\"></script>"+
+        "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
+      else
+        "<script type='text/javascript' src='#{path}'></script>"
+      end
     end
   end
 
