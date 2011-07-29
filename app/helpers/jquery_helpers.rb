bfac6642f3ff27a173ab1e3a048fb561887b91f0
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-29 12:48:03 +0800

    Use Google's CDN for jQuery even on development.

--- app/helpers/jquery_helpers.rb
+++ app/helpers/jquery_helpers.rb
@@ -1,24 +1,16 @@
 class Main
   module JqueryHelpers
     def jquery(path='/js/jquery.js', version='1.4.3')
-      if settings.production?
-        # Grab Google CDN's jQuery; fallback to local if necessary
-        url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
-        "<script type='text/javascript' src=\"#{url}\"></script>"+
-        "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
-      else
-        "<script type='text/javascript' src='#{path}'></script>"
-      end
+      # Grab Google CDN's jQuery; fallback to local if necessary
+      url = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js"
+      "<script type='text/javascript' src=\"#{url}\"></script>"+
+      "<script>!window.jQuery && document.write('<script src=\"#{path}\"><\\/script>')</script>"
     end
 
     def jquery_ui(path='/js/jqueryui.js', version='1.8.5')
-      if settings.production?
-        url = "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
-        "<script type='text/javascript' src=\"#{url}\"></script>"+
-        "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
-      else
-        "<script type='text/javascript' src='#{path}'></script>"
-      end
+      url = "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js"
+      "<script type='text/javascript' src=\"#{url}\"></script>"+
+      "<script>!window.jQuery.ui && document.write('<script src=\"#{path}\"><\\/script>')</script>"
     end
   end
 
